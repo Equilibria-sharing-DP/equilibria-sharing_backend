d8db5e2bb382b17f0d6b4dd26404836dffd76e3e
Manuel Fellner <mfellner@student.tgm.ac.at>
2025-03-02 12:55:16 +0100

    feat(AccommodationController)!: adding controller for managing accommodations (CRUD operations!), only accessible for authenticated employees

---
+++ src/main/java/api/equilibria_sharing/controller/AccommodationController.java
@@ -0,0 +1,164 @@
+package api.equilibria_sharing.controller;
+import api.equilibria_sharing.model.*;
+import api.equilibria_sharing.model.requests.*;
+import api.equilibria_sharing.repositories.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+/**
+ * Accommodation Controller - manage all Accommodation entries
+ *
+ * @author Manuel Fellner
+ * @version 02.03.2025
+ */
+@RestController
+@RequestMapping("/api/v1/accommodations")
+@PreAuthorize("isAuthenticated()")
+public class AccommodationController {
+    private static final Logger log = LoggerFactory.getLogger(AccommodationController.class);
+
+
+    private final AccommodationRepository accommodationRepository;
+    private final AddressRepository addressRepository;
+
+
+    public AccommodationController(AccommodationRepository accommodationRepository, AddressRepository addressRepository) {
+        this.accommodationRepository = accommodationRepository;
+        this.addressRepository = addressRepository;
+    }
+
+    /**
+     * createAccommodation Method- Takes a POST Request from the employee frontend part and saves & processes the form data from employee,
+     * this method is protected by authentication / authorization structure, only an employee can create a new accommodation!
+     * @param accommodationRequest request by frontend
+     * @return Status OK and new object in response body
+     */
+    @PostMapping
+    public ResponseEntity<Accommodation> createAccommodation(@RequestBody AccommodationRequest accommodationRequest)  {
+        log.info("Creating a new accommodation...");
+        log.info("Initializing Accommodation Address...");
+        // initialize address from request
+        Address address = new Address();
+        address.setCity(accommodationRequest.getCity());
+        address.setCountry(accommodationRequest.getCountry());
+        address.setPostalCode(accommodationRequest.getPostalCode());
+        address.setStreet(accommodationRequest.getStreet());
+        address.setHouseNumber(accommodationRequest.getHouseNumber());
+        address.setAddressAdditional(accommodationRequest.getAddressAdditional());
+        addressRepository.save(address);
+        log.info("Address: " + address);
+
+        // initializing accommodation entity from request
+        Accommodation accommodation = new Accommodation();
+        accommodation.setAddress(address);
+        accommodation.setName(accommodationRequest.getName());
+        accommodation.setType(accommodationRequest.getType());
+        accommodation.setDescription(accommodationRequest.getDescription());
+        accommodation.setMaxGuests(accommodationRequest.getMaxGuests());
+        accommodation.setPricePerNight(accommodationRequest.getPricePerNight());
+        accommodation.setPictureUrls(accommodationRequest.getPictureUrls());
+        accommodationRepository.save(accommodation);
+
+        log.info("Initialized following accommodation entity:" + accommodation);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(accommodation);
+    }
+
+    /**
+     * Get all Accommodations - Employee must be authenticated
+     * @return Status OK and accommodations as JSON in resp body
+     */
+    @GetMapping
+    public ResponseEntity<List<Accommodation>> getAllAccommodations() {
+        log.info("Fetching all Accommodations");
+        List<Accommodation> accommodations = accommodationRepository.findAll();
+        return ResponseEntity.ok(accommodations);
+    }
+
+    /**
+     * Get accommodation with specific ID - Employee must be authenticated
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Accommodation> getAccommodationById(@PathVariable("id") Long id) {
+        log.info("Fetching accommodation with id: " + id);
+        Accommodation accommodation = accommodationRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Booking not found"));
+        return ResponseEntity.ok(accommodation);
+    }
+
+    /**
+     * Delete ALL accommodations - Employee must be authenticated
+     * @return ok
+     */
+    @DeleteMapping
+    public ResponseEntity<Accommodation> deleteAllAccommodations() {
+        log.warn("Deleting all accommodations!");
+        accommodationRepository.deleteAll();
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * Delete specific accommodation with ID - Employee must be authenticated
+     * @param id id of accommodation to be deleted
+     * @return ok
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Accommodation> deleteAccommodationById(@PathVariable("id") Long id) {
+        log.warn("Deleting accommodation with id: " + id);
+        accommodationRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * Update an existing Accommodation object with new data - Employee must be authenticated
+     * @param id id of existing accommodation object
+     * @param accommodationRequest updated accommodation data
+     * @return OK with new object as JSON in resp. body
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<Accommodation> updateAccommodationById(@PathVariable("id") Long id, @RequestBody AccommodationRequest accommodationRequest) {
+        log.info("Updating accommodation with id: " + id);
+        // fetching existing accommodation
+        Accommodation existingAccommodation = accommodationRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Booking not found"));
+
+        // initialize address from request
+        Address address = new Address();
+        address.setCity(accommodationRequest.getCity());
+        address.setCountry(accommodationRequest.getCountry());
+        address.setPostalCode(accommodationRequest.getPostalCode());
+        address.setStreet(accommodationRequest.getStreet());
+        address.setHouseNumber(accommodationRequest.getHouseNumber());
+        address.setAddressAdditional(accommodationRequest.getAddressAdditional());
+        addressRepository.save(address);
+        log.info("Address: " + address);
+
+        // updating accommodation object
+        existingAccommodation.setName(accommodationRequest.getName());
+        existingAccommodation.setType(accommodationRequest.getType());
+        existingAccommodation.setDescription(accommodationRequest.getDescription());
+        existingAccommodation.setMaxGuests(accommodationRequest.getMaxGuests());
+        existingAccommodation.setPictureUrls(accommodationRequest.getPictureUrls());
+        existingAccommodation.setAddress(address);
+
+        accommodationRepository.save(existingAccommodation);
+
+        return ResponseEntity.ok(existingAccommodation);
+
+    }
+
+
+
+
+
+
+}
---
+++ src/main/java/api/equilibria_sharing/model/requests/AccommodationRequest.java
@@ -0,0 +1,114 @@
+package api.equilibria_sharing.model.requests;
+
+import java.util.List;
+
+public class AccommodationRequest {
+    private String name;
+    private String type;
+    private String description;
+    private String city;
+    private String country;
+    private int postalCode;
+    private String street;
+    private int houseNumber;
+    private String addressAdditional;
+    private int maxGuests;
+    private double pricePerNight;
+    private List<String> pictureUrls;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getAddressAdditional() {
+        return addressAdditional;
+    }
+
+    public void setAddressAdditional(String addressAdditional) {
+        this.addressAdditional = addressAdditional;
+    }
+
+    public int getMaxGuests() {
+        return maxGuests;
+    }
+
+    public void setMaxGuests(int maxGuests) {
+        this.maxGuests = maxGuests;
+    }
+
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public void setPricePerNight(double pricePerNight) {
+        this.pricePerNight = pricePerNight;
+    }
+
+    public List<String> getPictureUrls() {
+        return pictureUrls;
+    }
+
+    public void setPictureUrls(List<String> pictureUrls) {
+        this.pictureUrls = pictureUrls;
+    }
+}
